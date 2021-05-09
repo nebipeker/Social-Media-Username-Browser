521a51a5ad99bd5429975d86102ef57572209694
Egemen Iscan <egemen.iscan@ozu.edu.tr>
2021-05-09 19:56:22 +0300

    Update on social media selection

--- mainpage/main_page2.js
+++ mainpage/main_page2.js
@@ -1,1 +1,23 @@
-document.getElementById("demo").innerHTML = "Egemen";
+var comments = [{"account":"nebipeker","network":"instagram","message":"Great codes","commenter":"@egemeniscan"},{"account":"egemen","network":"instagram","message":"Great codes","commenter":"@egemeniscan"},{"account":"nebipeker","network":"twitter","message":"Good content","commenter":"@elonmusk"}];
+localStorage.setItem("comments", JSON.stringify(comments));
+
+function myfoo(){
+    localStorage.setItem("searchUsername",document.getElementById("searchUsername").value);
+}
+
+function reset(){
+    localStorage.setItem("searchUsername","default");
+}
+
+function instagram(){
+    localStorage.setItem("searchUsername","instagram");
+}
+
+function twitter(){
+    localStorage.setItem("searchUsername","twitter");
+}
+
+function youtube(){
+    localStorage.setItem("searchUsername","youtube");
+}
+
