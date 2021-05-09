a320a171b5c46db2c08919e557bae1765df6ebbe
Yiğit Demirşan <59603439+YigitDemirsan@users.noreply.github.com>
2021-05-09 18:12:50 +0300

    Update register_page.js
    Last modifications for checking the user info and add the password information to the local storage.

--- register/register_page.js
+++ register/register_page.js
@@ -1,27 +1,28 @@
 var usernamesArr = [];
 var emailsOfUsers = [];
+var passwordList = [];
 function saveInfo() {
     var username = document.getElementById("username");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password").value;
+    var repeated_password = document.getElementById("repeated_password").value;
     if(usernamesArr.includes(username.value)){
         alert("This user name already taken \n Please enter a new infos...")
     }else if(emailsOfUsers.includes(email.value)){
         alert("This email adress already using for one account..")
+    }else if(password != repeated_password){
+        alert("Password doesn't match.\nPlease enter the correct password.");
     }else{
         usernamesArr.push(username.value);
         emailsOfUsers.push(email.value);
         localStorage.setItem("usernames", JSON.stringify(usernamesArr));
         localStorage.setItem("emails", JSON.stringify(emailsOfUsers));
         alert("User registered successfully")
+        validatePassword();
     }
 }
 function validatePassword() {
-    var password = document.getElementById("password").value;
-    var repeated_password = document.getElementById("repeated_password").value;
-    if (password != repeated_password) {
-        alert("password doesn't match");
-        return false;
-    } 
-    localStorage.setItem("password", password.value);
-    localStorage.setItem("repeated_password", repeated_password.value);
-    return true;
+    var password = document.getElementById("password");
+    passwordList.push(password.value);
+    localStorage.setItem("passwords", JSON.stringify(passwordList));
 }
